f1e4ab90cef79072d12fc7ae9c9f9d8955f366b7
Windie Chai <windie@live.cn>
2016-04-20 17:50:47 +0800

    Create global.js

---
+++ global.js
@@ -0,0 +1,29 @@
+/* eslint no-magic-numbers: 0 */
+define("global", [], function () {
+    "use strict";
+
+    function ensure (parent, name, callback) {
+        var names = name.split("."),
+            actualName = names[0],
+            timeout = 100;
+
+        if (typeof parent[actualName] === "undefined") {
+            setTimeout(function () {
+                ensure(parent, name, callback);
+            }, timeout);
+        } else if (names.length === 1) {
+            callback(parent[actualName]);
+        } else {
+            names.shift();
+            ensure(parent[actualName], names.join("."), callback);
+        }
+    }
+
+    function load (name, req, onload) {
+        ensure(window, name, onload);
+    }
+
+    return {
+        load: load
+    };
+});
